f9439a207288bb8e3a6e32a4cb75563620970ee5
ninjaChickin <51749659+ninjaChickin@users.noreply.github.com>
2019-06-26 14:04:38 -0400

    just need to finish morse and debug!

--- MorseCipher.java
+++ MorseCipher.java
@@ -19,21 +19,43 @@ class MorseCipher extends Cipher{
   public String encrypt(String plaintext){
    plaintext.toUpperCase();
    String cryptText = "";
-   int count = 0;
    for (int i = 0; i < plaintext.length(); i++){
      if (Character.isWhitespace(plaintext.charAt(i))){// check if plaintext at i is a space
       cryptText += "       "; // add 7 spaces cuz new word
 
      }else{
        int de_index = alphabet.indexOf(plaintext.charAt(i));//find the index in codes that corresponds to the one in letters;
-       cryptText += codes[de_index] + "   "; //add it to newtext with 3 spaces
+       cryptText += (codes[de_index] + "   "); //add it to newtext with 3 spaces
      }
    }
    return cryptText;
  }
 
   public String decrypt(String cryptText) {
-    for (int i = 0;i < cryptText.length();i++)
+      String plainText = "";
+      String letterCheck = "";
+      int count = 0;
+      for (int i = 0; i < cryptText.length(); i++){
+          if (Character.isWhitespace(cryptText.charAt(i))){// check if plaintext at i is a space
+              //loop through codes to find the corresponding match in letters
+              if (letterCheck != ""){
+                  for (int j = 0;j < codes.length;j++){
+
+                  }
+                  letterCheck = "";//revert letterCheck back to normal
+              }
+              //removing the spaces
+              if (cryptText.substring(i,i + 6) == "       ") {// if it's a new word
+
+              }
+              else if (cryptText.substring(i,i + 2) == "   ") {// if it's normal space between letters
+
+              }
+
+          }else{
+              letterCheck += cryptText.charAt(i);
+          }
+      }
     return null;
   }
 }
