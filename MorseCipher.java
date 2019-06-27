14c882a34333b68a444a9363b59bd098ce900e44
BuildTools <unconfigured@null.spigotmc.org>
2019-06-27 08:45:49 -0400

    just debugging!

--- MorseCipher.java
+++ MorseCipher.java
@@ -17,16 +17,23 @@ class MorseCipher extends Cipher{
  public MorseCipher(){
  }
   public String encrypt(String plaintext){
-   plaintext.toUpperCase();
+   plaintext = plaintext.toUpperCase();
    String cryptText = "";
+   boolean did3space = false;
    for (int i = 0; i < plaintext.length(); i++){
-     if (Character.isWhitespace(plaintext.charAt(i))){// check if plaintext at i is a space
-      cryptText += "       "; // add 7 spaces cuz new word
-
-     }else{
-       int de_index = alphabet.indexOf(plaintext.charAt(i));//find the index in codes that corresponds to the one in letters;
-       cryptText += (codes[de_index] + "   "); //add it to newtext with 3 spaces
-     }
+       if (Character.isWhitespace(plaintext.charAt(i))){// check if plaintext at i is a space
+           if (did3space != true){cryptText += "       ";} // add 7 spaces cuz new word
+           else{cryptText += "    ";} // if you already did 3 spaces, only add 4 more
+           did3space = false;
+       }
+       else if (i + 1 == plaintext.length()){
+           int de_index = alphabet.indexOf(plaintext.charAt(i));//find the index in codes that corresponds to the one in letters;
+           cryptText += codes[de_index];} // if you reach the end, stop adding spaces
+       else{
+           int de_index = alphabet.indexOf(plaintext.charAt(i));//find the index in codes that corresponds to the one in letters; lol i put this in twice
+           cryptText += (codes[de_index] + "   "); //add it to newtext with 3 spaces
+           did3space = true;
+       }
    }
    return cryptText;
  }
@@ -34,28 +41,34 @@ class MorseCipher extends Cipher{
   public String decrypt(String cryptText) {
       String plainText = "";
       String letterCheck = "";
-      int count = 0;
       for (int i = 0; i < cryptText.length(); i++){
-          if (Character.isWhitespace(cryptText.charAt(i))){// check if plaintext at i is a space
-              //loop through codes to find the corresponding match in letters
-              if (letterCheck != ""){
-                  for (int j = 0;j < codes.length;j++){
-
-                  }
-                  letterCheck = "";//revert letterCheck back to normal
-              }
+          if (Character.isWhitespace(cryptText.charAt(i))){// check if cryptText at i is a space
               //removing the spaces
-              if (cryptText.substring(i,i + 6) == "       ") {// if it's a new word
-
+              if (i + 6 < cryptText.length()) {// if it's a new word
+                  if (cryptText.substring(i,i + 7) == "       "){
+                      plainText += " ";
+                      i += 6;
+                  }
               }
-              else if (cryptText.substring(i,i + 2) == "   ") {// if it's normal space between letters
-
+              else if (i + 2 < cryptText.length()) {// skip normal space between letters
+                  if (cryptText.substring(i,i + 3) == "   "){
+                      i += 2;
+                  }
               }
 
-          }else{
-              letterCheck += cryptText.charAt(i);
+          }
+          else{
+              letterCheck += cryptText.charAt(i);//add non-spaces to letterCheck
+          }
+          if (letterCheck != "" && (i + 1 == cryptText.length() || Character.isWhitespace(cryptText.charAt(i + 1)))){
+              for (int j = 0;j < codes.length;j++){
+                  if (letterCheck.equals(codes[j])){
+                      plainText += letters.charAt(j);
+                      letterCheck = "";//revert letterCheck back to normal, if used
+                  }
+              }
           }
       }
-    return null;
+    return plainText;
   }
 }
